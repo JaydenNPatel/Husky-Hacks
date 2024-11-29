a12b7103a9ee2ab4bdb8e6a4692169ad3e18df89
Jayden Patel <94498124+JaydenNPatel@users.noreply.github.com>
2024-11-29 11:14:17 -0500

    Update alert_type.sql

--- database-files/alert_type.sql
+++ database-files/alert_type.sql
@@ -1,1 +1,5 @@
-git
+INSERT INTO alert_type (type_id, type_name) VALUES (1, 'Error');
+INSERT INTO alert_type (type_id, type_name) VALUES (2, 'Warning');
+INSERT INTO alert_type (type_id, type_name) VALUES (3, 'Info');
+INSERT INTO alert_type (type_id, type_name) VALUES (4, 'Critical');
+INSERT INTO alert_type (type_id, type_name) VALUES (5, 'Notification');
