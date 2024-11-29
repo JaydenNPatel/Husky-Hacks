5271d8959f90366240c4ce0a27636ea3159bb526
Mmirza964 <mathewfmirza@gmail.com>
2024-11-29 16:50:12 -0500

    added user_engagement_metrics mock data

---
+++ database-files/user_engagement_metrics.sql
@@ -0,0 +1,97 @@
+use Husky_Hacks;
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (23.45, 1500, 200, 1300, 1);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (17.89, 1200, 180, 1020, 2);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (45.67, 2000, 500, 1500, 1);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (33.21, 1800, 300, 1500, 3);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (27.63, 1600, 220, 1380, 4);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (12.34, 1000, 150, 850, 2);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (54.21, 2500, 600, 1900, 5);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (30.56, 1800, 300, 1500, 3);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (41.67, 2100, 400, 1700, 6);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (37.89, 1900, 320, 1580, 1);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (20.45, 1400, 250, 1150, 7);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (25.78, 1500, 300, 1200, 8);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (32.89, 1600, 400, 1200, 9);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (29.45, 1400, 350, 1050, 1);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (38.23, 2000, 450, 1550, 10);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (44.67, 2200, 500, 1700, 5);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (18.56, 1300, 200, 1100, 7);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (24.78, 1500, 250, 1250, 6);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (35.67, 1800, 400, 1400, 8);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (47.89, 2400, 600, 1800, 9);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (21.34, 1300, 220, 1080, 10);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (36.45, 1900, 400, 1500, 11);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (30.23, 1700, 350, 1350, 12);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (50.78, 2600, 700, 1900, 13);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (43.89, 2200, 500, 1700, 14);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (27.45, 1600, 300, 1300, 9);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (31.56, 1800, 400, 1400, 6);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (42.34, 2100, 500, 1600, 5);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (26.89, 1500, 250, 1250, 8);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (33.23, 1700, 400, 1300, 9);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (39.78, 1900, 450, 1450, 5);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (48.45, 2400, 600, 1800, 6);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (35.67, 2000, 450, 1550, 7);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (40.56, 2200, 500, 1700, 4);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (23.45, 1400, 250, 1150, 3);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (28.78, 1600, 300, 1300, 2);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (34.23, 1800, 350, 1450, 1);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (29.45, 1700, 400, 1300, 1);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (41.56, 2100, 550, 1550, 2);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (37.89, 1900, 450, 1450, 3);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (22.78, 1400, 200, 1200, 9);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (31.89, 1700, 400, 1300, 8);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (49.23, 2600, 700, 1900, 5);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (45.67, 2400, 600, 1800, 6);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (38.45, 2000, 500, 1500, 3);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (32.56, 1800, 350, 1450, 1);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (30.89, 1600, 300, 1300, 2);
+INSERT INTO user_engagement_metrics (engagement_rate, active_users, new_users, returning_users, data_source)
+VALUES (25.67, 1500, 250, 1250, 4);
