ea150459a53ef4542925c6252e61f159eeaece90
Jayden Patel <94498124+JaydenNPatel@users.noreply.github.com>
2024-11-29 11:38:10 -0500

    Update alert_type.sql

--- database-files/alert_type.sql
+++ database-files/alert_type.sql
@@ -1,5 +1,10 @@
-INSERT INTO alert_type (type_id, type_name) VALUES (6, 'Project Feedback');
-INSERT INTO alert_type (type_id, type_name) VALUES (7, 'Data Integrity Alert');
-INSERT INTO alert_type (type_id, type_name) VALUES (8, 'System Performance Alert');
-INSERT INTO alert_type (type_id, type_name) VALUES (9, 'Coding Challenge Update');
-INSERT INTO alert_type (type_id, type_name) VALUES (10, 'User Engagement Alert');
+INSERT INTO alert_type (type_id, type_name) VALUES (1, 'Feedback Request');
+INSERT INTO alert_type (type_id, type_name) VALUES (2, 'New Project Assigned');
+INSERT INTO alert_type (type_id, type_name) VALUES (3, 'Report Submission');
+INSERT INTO alert_type (type_id, type_name) VALUES (4, 'Permission Update');
+INSERT INTO alert_type (type_id, type_name) VALUES (5, 'System Error');
+INSERT INTO alert_type (type_id, type_name) VALUES (6, 'Progress Review');
+INSERT INTO alert_type (type_id, type_name) VALUES (7, 'Archived Data Notice');
+INSERT INTO alert_type (type_id, type_name) VALUES (8, 'New Notification');
+INSERT INTO alert_type (type_id, type_name) VALUES (9, 'Alert Threshold Exceeded');
+INSERT INTO alert_type (type_id, type_name) VALUES (10, 'User Status Change');
