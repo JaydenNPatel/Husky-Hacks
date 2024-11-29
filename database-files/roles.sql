409b195e7bd7f81feaa21cbbbb12393df9a23d37
Jayden Patel <94498124+JaydenNPatel@users.noreply.github.com>
2024-11-28 23:00:35 -0500

    Update roles.sql

--- database-files/roles.sql
+++ database-files/roles.sql
@@ -1,4 +1,4 @@
-insert into roles (role_id, role_title) values (1, 'Web Designer II');
-insert into roles (role_id, role_title) values (2, 'General Manager');
-insert into roles (role_id, role_title) values (3, 'General Manager');
-insert into roles (role_id, role_title) values (4, 'Actuary');
+insert into roles (role_id, role_title) values (1, 'Student');
+insert into roles (role_id, role_title) values (2, 'Student');
+insert into roles (role_id, role_title) values (3, 'System Administrator');
+insert into roles (role_id, role_title) values (4, 'Data Analyst');
