4b62e17b3202d9f231b487d34600f5de386e3427
noelburra <noel.burra3@gmail.com>
2024-11-29 20:19:17 -0500

    updated

--- database-files/departments.sql
+++ database-files/departments.sql
@@ -1,40 +1,42 @@
-insert into departments (name) values ('placerat');
-insert into departments (name) values ('lacinia');
-insert into departments (name) values ('volutpat');
-insert into departments (name) values ('blandit');
-insert into departments (name) values ('nec');
-insert into departments (name) values ('sit');
-insert into departments (name) values ('pharetra');
-insert into departments (name) values ('fermentum');
-insert into departments (name) values ('ut');
-insert into departments (name) values ('vestibulum');
-insert into departments (name) values ('rutrum');
-insert into departments (name) values ('quis');
-insert into departments (name) values ('non');
-insert into departments (name) values ('erat');
-insert into departments (name) values ('in');
-insert into departments (name) values ('lobortis');
-insert into departments (name) values ('habitasse');
-insert into departments (name) values ('etiam');
-insert into departments (name) values ('habitasse');
-insert into departments (name) values ('interdum');
-insert into departments (name) values ('risus');
-insert into departments (name) values ('hendrerit');
-insert into departments (name) values ('vel');
-insert into departments (name) values ('nibh');
-insert into departments (name) values ('eget');
-insert into departments (name) values ('ut');
-insert into departments (name) values ('odio');
-insert into departments (name) values ('consequat');
-insert into departments (name) values ('nulla');
-insert into departments (name) values ('ullamcorper');
-insert into departments (name) values ('eget');
-insert into departments (name) values ('sollicitudin');
-insert into departments (name) values ('dui');
-insert into departments (name) values ('ligula');
-insert into departments (name) values ('nunc');
-insert into departments (name) values ('non');
-insert into departments (name) values ('quis');
-insert into departments (name) values ('porttitor');
-insert into departments (name) values ('ultrices');
-insert into departments (name) values ('ut');
+use Husky_Hacks;
+INSERT INTO departments (name) VALUES
+('Engineering'),
+('Marketing'),
+('Finance'),
+('Human Resources'),
+('Information Technology'),
+('Sales'),
+('Customer Support'),
+('Legal'),
+('Operations'),
+('Product Development'),
+('Quality Assurance'),
+('Supply Chain'),
+('Procurement'),
+('Public Relations'),
+('Data Analytics'),
+('Cybersecurity'),
+('Business Development'),
+('Corporate Strategy'),
+('Risk Management'),
+('Compliance'),
+('Investor Relations'),
+('Facilities Management'),
+('Training and Development'),
+('Event Planning'),
+('Content Management'),
+('Social Media Marketing'),
+('Graphic Design'),
+('Research and Development'),
+('Environmental Sustainability'),
+('Manufacturing'),
+('Retail Operations'),
+('Technical Writing'),
+('Customer Success'),
+('DevOps'),
+('Field Services'),
+('Technical Support'),
+('Digital Transformation'),
+('Software Implementation'),
+('Hardware Engineering'),
+('Cloud Infrastructure');
