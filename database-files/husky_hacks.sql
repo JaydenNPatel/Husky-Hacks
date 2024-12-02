d068bb0c77fdd6f6078bcf32394d015b39be6cb0
Jayden Patel <94498124+JaydenNPatel@users.noreply.github.com>
2024-12-02 15:35:28 -0500

    Update husky_hacks.sql

--- database-files/husky_hacks.sql
+++ database-files/husky_hacks.sql
@@ -302,3 +302,8 @@ CREATE TABLE kpi_views (
     UNIQUE INDEX uq_idx_id (view_id),
     INDEX idx_created_by (created_by)
 );
+
+insert into users (user_id, first_name, last_name, email, role, status, date_created) values (0001, 'Alex', 'Chen', 'calex@ning.com', 1, true, '2/1/2024');
+insert into users (user_id, first_name, last_name, email, role, status, date_created) values (0002, 'Sally', 'Pigott', 'spigs@blog.com', 2, true, '4/12/2024');
+insert into users (user_id, first_name, last_name, email, role, status, date_created) values (0003, 'Saquon', 'Porkly', 'saquon@opera.com', 3, false, '7/15/2024');
+insert into users (user_id, first_name, last_name, email, role, status, date_created) values (0004, 'Devin', 'Codington', 'coder@wikispaces.com', 4, false, '3/7/2024');
