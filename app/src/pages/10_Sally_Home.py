8f662bda2575763841fc44dbae39c589ab7e4d39
Jayden Patel <94498124+JaydenNPatel@users.noreply.github.com>
2024-12-04 17:41:44 -0500

    Update 10_Sally_Home.py

--- app/src/pages/10_Sally_Home.py
+++ app/src/pages/10_Sally_Home.py
@@ -15,3 +15,7 @@ if st.button("Review Feedback"):
 if st.button("Track Progress"):
     st.write("Redirecting to Track Progress page...")
     st.experimental_rerun()
+
+if st.button("Back to Home"):
+    st.session_state['authenticated'] = False
+    st.switch_page('ProjectHome.py') 
