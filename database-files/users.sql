72c0d6d9fdd65ee15f11f448849a9a1ff06e1b2b
rikinr5 <132939646+rikinr5@users.noreply.github.com>
2024-12-04 01:27:14 -0500

    Delete database-files/users.sql

--- database-files/users.sql
+++
@@ -1,5 +0,0 @@
-use Husky_Hacks;
-insert into users (user_id, first_name, last_name, email, role, status, date_created) values (0001, 'Alex', 'Chen', 'calex@ning.com', 1, true, '2/1/2024');
-insert into users (user_id, first_name, last_name, email, role, status, date_created) values (0002, 'Sally', 'Pigott', 'spigs@blog.com', 2, true, '4/12/2024');
-insert into users (user_id, first_name, last_name, email, role, status, date_created) values (0003, 'Saquon', 'Porkly', 'saquon@opera.com', 3, false, '7/15/2024');
-insert into users (user_id, first_name, last_name, email, role, status, date_created) values (0004, 'Devin', 'Codington', 'coder@wikispaces.com', 4, false, '3/7/2024');
