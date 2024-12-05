ff13238eb82bc67a5b88b8c1536d97d9a4f65895
Mmirza964 <mathewfmirza@gmail.com>
2024-12-05 00:32:11 -0500

    added button for user engagement metrics

--- app/src/pages/31_Devin_View_Metrics.py
+++ app/src/pages/31_Devin_View_Metrics.py
@@ -26,3 +26,13 @@ if st.button("Revenue Metrics"):
     else:
         st.write("Failed to get data")
         #st.error(f"Failed to fetch items. Status code: {response.status_code}")
+
+if st.button("User Engagement Metrics"):
+    response = requests.get(f"{BASE_URL}/devin/user_engagement_metrics")
+
+    if response.status_code == 200:
+        items = response.json()  # Convert JSON response to Python dictionary/list
+        st.write("Items fetched from API:", items)
+    else:
+        st.write("Failed to get data")
+        #st.error(f"Failed to fetch items. Status code: {response.status_code}")
