019d357e947ed9f7cc30ae1504e58b9c001c85a0
Jayden Patel <jaydenpatel057@gmail.com>
2024-12-05 01:15:38 -0500

    fixed home buttom to send to project home, and update the about tab

--- app/src/modules/nav.py
+++ app/src/modules/nav.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 #### ------------------------ General ------------------------
 def HomeNav():
-    st.sidebar.page_link("pages/Home.py", label="Home", icon="🏠")
+    st.sidebar.page_link("pages/ProjectHome.py", label="Home", icon="🏠")
 
 
 def AboutPageNav():
