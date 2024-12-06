6e71d7386da60a6775267d7aec9e23363b3868b6
Mmirza964 <mathewfmirza@gmail.com>
2024-12-05 19:45:54 -0500

    added buttons for kpi views in devin

--- app/src/pages/32_Kpi_Views.py
+++ app/src/pages/32_Kpi_Views.py
@@ -8,5 +8,14 @@ BASE_URL = os.getenv("BASE_URL", "http://web-api:4000")
 
 st.title("Create KPI Views")
 
+if st.button("All Views"):
+    st.switch_page("pages/30_Devin_Home.py")
+
+if st.button("Create New View"):
+    st.switch_page("pages/30_Devin_Home.py")
+
+if st.button("Edit Existing View"):
+    st.switch_page("pages/30_Devin_Home.py")
+
 if st.button("Back To Menu"):
     st.switch_page("pages/30_Devin_Home.py")
