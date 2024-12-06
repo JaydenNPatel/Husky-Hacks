4ae5e5cce6c90bfa3badb2be358be8838a912443
Mmirza964 <mathewfmirza@gmail.com>
2024-12-05 21:37:22 -0500

    added new route for devin to get all alerts

--- app/src/pages/33_Devin_Alerts.py
+++ app/src/pages/33_Devin_Alerts.py
@@ -4,4 +4,8 @@ import os
 
 BASE_URL = os.getenv("BASE_URL", "http://web-api:4000") 
 
-st.title("Metrics Dashboard")
+st.title("Current Notifications")
+
+if st.button("Back To Menu"):
+    st.switch_page('pages/30_Devin_Home.py')
+
