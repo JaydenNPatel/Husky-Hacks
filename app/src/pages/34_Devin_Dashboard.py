279f42aa3ae93508b5a837b4f12c4f7d3767ff9e
Mmirza964 <mathewfmirza@gmail.com>
2024-12-05 23:54:02 -0500

    added a dashboard page for devin

---
+++ app/src/pages/34_Devin_Dashboard.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import requests
+import os
+
+BASE_URL = os.getenv("BASE_URL", "http://web-api:4000") 
+
+st.title("Current Notifications")
+
+if st.button("Back To Menu"):
+    st.switch_page('pages/30_Devin_Home.py')
